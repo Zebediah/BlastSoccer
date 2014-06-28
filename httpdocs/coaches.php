e1c6e41d453f64434c99dd439e66c44a26546d00
Andrew Harderson <sir.clayton@gmail.com>
2014-06-27 20:11:16 -0600

    Initial Master Commit

---
+++ httpdocs/coaches.php
@@ -0,0 +1,68 @@
+<html>
+<head>
+	<title>Blast Soccer FC | </title>
+	<link rel="stylesheet" href="/css/site.css" type="text/css">
+	<script src="/js/site.js"></script>
+	<link rel = stylesheet type = "text/css" href = "/css/jquery.openCarousel.css">
+	<script src="http://code.jquery.com/jquery-1.9.1.min.js"></script> 
+	<script type="text/javascript" src="/js/jquery.openCarousel.js"></script>
+</head>
+<body>
+
+<div class="main">
+	<div class="uber-nav">
+		<a href="/contact.php">CONTACT BLAST FC</a>
+	</div>
+	<div class="header-wrap">
+		<div class="header">
+			<div class="logo">
+				<a href="index.html"><img src="/images/logo.png" border="0" alt="Blast Soccer FC" /></a>
+			</div>
+			<div class="navigation">
+				<ul>
+					<li><a href="/about.php">ABOUT BLAST FC</a></li>
+					<li><a href="/teams.php">TEAMS</a></li>
+					<li><a href="/coaches.php">COACHES</a></li>
+					<li><a href="/academy.php">ACADEMY</a></li>
+					<li><a href="/registration.php">REGISTRATION</a></li>
+					<li><a href="/fees.php">FEES</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<div class="content-wrap">
+		<!-- CONTENT -->
+		<div class="content">
+			
+			<div class="hero_wrap">
+			    <div class="hero">
+					<!-- SLIDE 2 -->
+				    <div class="content">
+				        <h3>COACHES</h3>
+				        <p>
+				    		More information coming soon!
+				        </p>
+				    </div>
+			    </div>
+		    </div>
+
+		</div>
+		<!-- END CONTENT -->
+	</div>
+	<div style="clear: both;"> </div>
+	<div class="footer">
+		<div class="content">
+			<div class="links">
+				<a href="/contact.php">CONTACT BLAST FC</a>
+			</div>
+			<div class="copyright">
+				&copy; 2014 Blast Soccer Club
+			</div>
+			<div style="clear: both;"> </div>
+		</div>
+
+	</div>
+</div>
+
+</body>
+</html>
---
+++ httpdocs/contact.php
@@ -0,0 +1,117 @@
+<html>
+<head>
+	<title>Blast Soccer FC | </title>
+	<link rel="stylesheet" href="/css/site.css" type="text/css">
+	<script src="/js/site.js"></script>
+	<link rel = stylesheet type = "text/css" href = "/css/jquery.openCarousel.css">
+	<script src="http://code.jquery.com/jquery-1.9.1.min.js"></script> 
+	<script type="text/javascript" src="/js/jquery.openCarousel.js"></script>
+</head>
+<body>
+
+<div class="main">
+	<div class="uber-nav">
+		<a href="/contact.php">CONTACT BLAST FC</a>
+	</div>
+	<div class="header-wrap">
+		<div class="header">
+			<div class="logo">
+				<a href="index.html"><img src="/images/logo.png" border="0" alt="Blast Soccer FC" /></a>
+			</div>
+			<div class="navigation">
+				<ul>
+					<li><a href="/about.php">ABOUT BLAST FC</a></li>
+					<li><a href="/teams.php">TEAMS</a></li>
+					<li><a href="/coaches.php">COACHES</a></li>
+					<li><a href="/academy.php">ACADEMY</a></li>
+					<li><a href="/registration.php">REGISTRATION</a></li>
+					<li><a href="/fees.php">FEES</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<div class="content-wrap">
+		<!-- CONTENT -->
+		<div class="content">
+			
+			<div class="hero_wrap">
+			    <div class="hero">
+					<!-- SLIDE 2 -->
+				    <div class="content">
+				        <h3>CONTACT</h3>
+				        <div class="left">
+				     		<p>
+				    			<b>SHAWN KENNEDY</b> - DIRECTOR<br />
+				    			2196 E. Arbor Lane<br />
+				    			Holladay, UT 84117<br />
+				    			801.259.0577 (cell)<br />
+				    			801.424.2939 (home)<br />
+				    			<a href="mailto:shawnkennedy@xmission.com">shawnkennedy@xmission.com</a>
+				       		</p>
+
+				       		<p>
+				    			<b>MERRILEE JENSEN</b> - SECRETARY/REGISTRAR<br />
+				    			3215 Bonview Drive<br />
+				    			Salt Lake City, UT 84109<br />
+				    			801.860.3896 (cell)<br />
+				    			801.907.4488 (home)<br />
+				    			<a href="mailto:mer5jensen@hotmail.com">mer5jensen@hotmail.com</a>
+				       		</p>
+
+				       		<p>
+				    			<b>JOEY KENNEDY</b> - BOOKEEPER<br />
+				    			2196 E. Arbor Lane<br />
+				    			Holladay, UT 84117<br />
+				    			801.634.8878 (cell)<br />
+				    			801.424.2939 (home)<br />
+				    			<a href="mailto:shawnkennedy@xmission.com">shawnkennedy@xmission.com</a>
+				       		</p>
+
+				    	</div>
+				    	<div class="right">
+				    		<p>
+				    			<b>HOLLY LITTLE</b> - TREASURER<br />
+				    			4024 Mercury Drive<br />
+				    			Salt Lake City, UT 84124<br />
+				    			801.860.3896 (cell)<br />
+				    			801.907.4488 (home)<br />
+				    			<a href="mailto:clittle121819@msn.com">clittle121819@msn.com</a>
+				       		</p>
+
+				       		<p>
+				    			<b>ROBERT HOWARTH</b> - FIELD COORDINATOR<br />
+				    			2089 Sahara Drive<br />
+				    			Salt Lake City, UT 84117<br />
+				    			801.750.1656 (cell)<br />
+				    			801.274.2371 (home)<br />
+				    			<a href="mailto:robertellery@msn.com">robertellery@msn.com</a>
+				       		</p>
+
+				       		
+				    	
+				    	</div>
+				    	<div style="clear: both;"> </div>
+				    </div>
+			    </div>
+		    </div>
+
+		</div>
+		<!-- END CONTENT -->
+	</div>
+	<div style="clear: both;"> </div>
+	<div class="footer">
+		<div class="content">
+			<div class="links">
+				<a href="/contact.php">CONTACT BLAST FC</a>
+			</div>
+			<div class="copyright">
+				&copy; 2014 Blast Soccer Club
+			</div>
+			<div style="clear: both;"> </div>
+		</div>
+
+	</div>
+</div>
+
+</body>
+</html>
